aa122a0c373902a6eb74f31fd05d2f937c01ca91
Maurici Abad <hello@mauriciabad.com>
2022-03-22 23:18:25 +0100

    Add player text

--- src/views/DealerView.vue
+++ src/views/DealerView.vue
@@ -6,8 +6,10 @@
 
 <style scoped lang="scss">
 .dealer {
+  text-align: center;
   min-height: 100vh;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 }
--- src/views/PlayerView.vue
+++ src/views/PlayerView.vue
@@ -1,13 +1,16 @@
 <template>
   <main class="player">
-    <h1>You are the player</h1>
+    <h1>You are a player</h1>
+    <p>Open your camera and scan the QR code that the dealer will show you.</p>
   </main>
 </template>
 
 <style scoped lang="scss">
 .player {
+  text-align: center;
   min-height: 100vh;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 }
