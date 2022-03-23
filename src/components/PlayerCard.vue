8e5d24239b9de8c8a0cf668f0f9d2cb75eb54059
Maurici Abad <hello@mauriciabad.com>
2022-03-23 04:39:14 +0100

    Add QR code

--- src/components/PlayerCard.vue
+++ src/components/PlayerCard.vue
@@ -2,23 +2,38 @@
 import type { PlayerConfig } from "@/stores/gameConfig";
 import { getCharacter } from "@/data/characters";
 import { getAbility } from "@/data/abilities";
+import { computed } from "vue";
+import QrcodeVue from "qrcode.vue";
 
 interface Props {
   player: PlayerConfig;
 }
 
-defineProps<Props>();
+const props = defineProps<Props>();
+
+const url = computed<string>(() => {
+  const { character, abilities } = props.player;
+  const url = new URL(`${location.host}/player`);
+  url.searchParams.append("character", character);
+  url.searchParams.append("abilities", abilities.join(","));
+
+  return url.toString();
+});
 </script>
 
 <template>
   <div class="player">
-    <div>Name: {{ player.name }}</div>
-    <div>Character: {{ getCharacter(player.character).name }}</div>
+    <div class="name">Name: {{ player.name }}</div>
+    <div class="character">Character: {{ getCharacter(player.character).name }}</div>
 
-    <div>Abilities:</div>
-    <ul>
-      <li v-for="abilityId in player.abilities" :key="abilityId">{{ getAbility(abilityId).name }}</li>
-    </ul>
+    <div class="abilities">
+      <div>Abilities:</div>
+      <ul>
+        <li v-for="abilityId in player.abilities" :key="abilityId">{{ getAbility(abilityId).name }}</li>
+      </ul>
+    </div>
+
+    <qrcode-vue class="qr" render-as="svg" :value="url" :margin="2" />
   </div>
 </template>
 
@@ -34,5 +49,25 @@ defineProps<Props>();
   width: 100%;
   margin: 1rem auto 0;
   text-align: left;
+
+  display: grid;
+  grid-template: auto auto auto / 1fr auto;
+  grid-template-areas: "name qr" "character qr" "abilities qr";
+  gap: 0.5rem;
+}
+
+.name {
+  grid-area: name;
+}
+.character {
+  grid-area: character;
+}
+.abilities {
+  grid-area: abilities;
+}
+.qr {
+  grid-area: qr;
+  width: 100px;
+  align-self: center;
 }
 </style>
