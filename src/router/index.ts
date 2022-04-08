289160f8fd0bfb4c86a8b8bbbde31fb99dd0c7fa
Maurici Abad <hello@mauriciabad.com>
2022-04-08 22:26:51 +0200

    Scroll to top when route changes

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,10 @@ import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior: (to, from, savedPosition) => {
+    if (savedPosition) return savedPosition
+    return { top: 0, left: 0 }
+  },
   routes: [
     {
       path: '/',
