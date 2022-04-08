e507f9ade9a2137b2d4bf4252e7798592fe5508c
Maurici Abad <hello@mauriciabad.com>
2022-04-08 22:32:19 +0200

    Add actions for the major

--- src/data/actions.ts
+++ src/data/actions.ts
@@ -2,37 +2,46 @@ import type { Action } from './actions.types'
 
 export const firstNightActions: Action[] = [
   {
+    name: 'Major',
+    description: 'Major is revealed.',
+    requiredAbilities: ['major'],
+  },
+  {
+    name: 'Village goes to sleep',
+    description: 'Everyone close their eyes.',
+  },
+  {
     name: 'Cupid',
-    description: 'Cupid chooses the lovers and they look at each other',
+    description: 'Cupid chooses the lovers and they look at each other.',
     requiredAbilities: ['cupid'],
   },
   {
     name: 'Feral kid',
-    description: 'Feral kid picks an adoptive parent',
+    description: 'Feral kid picks an adoptive parent.',
     requiredCharacters: ['feral-kid'],
   },
   {
     name: 'Triplets',
-    description: 'Triplets see each other',
+    description: 'Triplets see each other.',
     requiredCharacters: ['triplet'],
   },
   {
     name: 'Twins',
-    description: 'Twins see each other',
+    description: 'Twins see each other.',
     requiredCharacters: ['twin'],
   },
   {
     name: 'Werewolfs see each other',
-    description: 'Werewolfs just see each other, no killing',
+    description: 'Werewolfs just see each other, no killing.',
     requiredCharacters: ['werewolf'],
   },
   {
     name: 'Village wakes up',
-    description: 'Everyone opens their eyes',
+    description: 'Everyone opens their eyes.',
   },
   {
     name: 'Nudist',
-    description: 'Nudist is revealed',
+    description: 'Nudist is revealed (gets naked metaphorically).',
     requiredCharacters: ['nudist'],
   },
 ]
@@ -40,27 +49,27 @@ export const firstNightActions: Action[] = [
 export const nightActions: Action[] = [
   {
     name: 'Seer',
-    description: 'Seer picks a player to look at and sees their role',
+    description: 'Seer picks a player to look at and sees their role.',
     requiredCharacters: ['seer'],
   },
   {
     name: 'Werewolfs kill',
-    description: 'Werewolfs vote someone to kill',
+    description: 'Werewolfs vote someone to kill.',
     requiredCharacters: ['werewolf'],
   },
   {
     name: 'Witch',
-    description: 'Witch chooses to save or kills a player',
+    description: 'Witch chooses to save or kills a player.',
     requiredCharacters: ['witch'],
   },
   {
     name: 'Doctor',
-    description: 'Witch chooses to save player',
+    description: 'Witch chooses to save player.',
     requiredCharacters: ['doctor'],
   },
   {
     name: 'Baker',
-    description: 'Baker gives bread to a player and reveals his role',
+    description: 'Baker gives bread to a player and reveals his role.',
     requiredCharacters: ['baker'],
   },
 ]
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ import RealVh from 'real-vh'
 import App from './App.vue'
 import router from './router'
 
-const STORAGE_VERSION = 1
+const STORAGE_VERSION = 2
 
 const lastAppVersion = parseInt(localStorage.getItem('appVersion') ?? '', 10)
-if (isNaN(lastAppVersion) || lastAppVersion < STORAGE_VERSION) {
+if (lastAppVersion !== STORAGE_VERSION) {
   localStorage.clear()
 }
 localStorage.setItem('appVersion', STORAGE_VERSION.toString())
