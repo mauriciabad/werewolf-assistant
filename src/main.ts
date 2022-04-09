0bfa4ba2170d8ed3a396085c32f7d30df0c47e4e
Maurici Abad <hello@mauriciabad.com>
2022-04-09 09:19:50 +0200

    Update storage version

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,13 @@ import RealVh from 'real-vh'
 import App from './App.vue'
 import router from './router'
 
-const STORAGE_VERSION = 2
+const STORAGE_VERSION = 3
 
-const lastAppVersion = parseInt(localStorage.getItem('appVersion') ?? '', 10)
-if (lastAppVersion !== STORAGE_VERSION) {
+const lastStorageVersion = parseInt(
+  localStorage.getItem('appVersion') ?? '',
+  10
+)
+if (lastStorageVersion !== STORAGE_VERSION) {
   localStorage.clear()
 }
 localStorage.setItem('appVersion', STORAGE_VERSION.toString())
