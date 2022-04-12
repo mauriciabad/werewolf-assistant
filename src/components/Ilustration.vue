709ce6596b285c9e8a2536f5dbb169135a678ae9
Maurici Abad <hello@mauriciabad.com>
2022-04-12 20:19:57 +0200

    Ilustration spinner

--- src/components/Ilustration.vue
+++ src/components/Ilustration.vue
@@ -1,11 +1,29 @@
 <script setup lang="ts">
 import ilustrations, { type IlustrationId } from '@/data/ilustrations'
+import { ref } from 'vue'
 
 defineProps<{
   id: IlustrationId
 }>()
+
+const isLoading = ref<boolean>(true)
 </script>
 
 <template>
-  <img :src="ilustrations[id]" alt="" loading="lazy" />
+  <img
+    :src="ilustrations[id]"
+    alt=""
+    loading="lazy"
+    :class="{ loading: isLoading }"
+    @load="isLoading = false"
+  />
 </template>
+
+<style lang="scss" scoped>
+.loading {
+  background-image: url('@/assets/loading-spinner.svg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: 75%;
+}
+</style>
