b176989ff1636a615d76c3eb3903f457dfc96010
Maurici Abad <hello@mauriciabad.com>
2022-04-13 16:41:50 +0200

    Fix i18n console warning

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,10 +1,9 @@
 import { createI18n } from 'vue-i18n'
-import ca from '@/locales/ca.json'
-import en from '@/locales/en.json'
-import es from '@/locales/es.json'
 import flagCA from '@/assets/flags/flagCA.svg'
 import flagEN from '@/assets/flags/flagEN.svg'
 import flagES from '@/assets/flags/flagES.svg'
+
+import messages from '@intlify/vite-plugin-vue-i18n/messages'
 
 const i18n = createI18n({
   locale: navigator.language,
@@ -13,11 +12,7 @@ const i18n = createI18n({
     ca: ['es'],
     default: ['en'],
   },
-  messages: {
-    ca,
-    en,
-    es,
-  },
+  messages,
   fallbackWarn: false,
   missingWarn: false,
 })
